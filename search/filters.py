89f5f17cd63bb8bddb7ab15a7c6088268049e1cf
Camille Huang <camille_huang@berkeley.edu>
2022-04-05 23:39:52 -0700

    Added styling using bootstrap3

--- search/filters.py
+++ search/filters.py
@@ -5,7 +5,7 @@ class PoseFilter(django_filters.FilterSet):
     protein__virus_name = django_filters.AllValuesFilter(label="Virus Name")
     protein__protein_name = django_filters.AllValuesFilter(label="Protein Name")
     ligand__zinc_id = django_filters.CharFilter(label="Ligand ZINC ID")
-    binding_affinity = django_filters.RangeFilter(label="Binding Affinity")
+    binding_affinity = django_filters.RangeFilter(label="Binding Affinity Range", widget=django_filters.widgets.RangeWidget(attrs={'placeholder': 'Binding affinity'}))
     class Meta:
         model = Pose
         fields = ['protein__virus_name', 'protein__protein_name', 'ligand__zinc_id', 'binding_affinity']
--- search/urls.py
+++ search/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('filtered-tables-test/', views.filtered_pose_table, name='filtered-tables'),
-    path('results/download/<filename>', views.render_download, name='download'),
+    path('', views.filtered_pose_table, name='home'),
+    path('download/<filename>', views.render_download, name='download'),
 ]
